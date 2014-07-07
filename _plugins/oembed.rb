d6642c13d02bad3d4a172910d1a4df3c04648053
Colin <colin.calnan@gmail.com>
2014-07-06 19:45:59 -0700

    Updating the homepage and finishing the kids TV draft

--- _plugins/oembed.rb
+++ _plugins/oembed.rb
@@ -3,13 +3,13 @@
 #   - requires https://github.com/judofyr/ruby-oembed/
 #
 # Copyright 2011 Tammo van Lessen
-# 
+#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
-# 
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,18 +32,18 @@ module Jekyll
        super
        @text = text
     end
-    
+
     def render(context)
       # pipe param through liquid to make additional replacements possible
       url = Liquid::Template.parse(@text).render context
 
       # oembed look up
       result = ::OEmbed::Providers.get(url.strip!, :format => :xml)
-      
+
       # Odd: slideshare uses provider-name instead of provider_name
       provider = result.fields['provider_name'] || result.fields['provider-name'] || 'unknown'
 
-      "<div class=\"embed #{result.type} #{provider}\">#{result.html}</div>"
+      "<div class=\"flex-video embed #{result.type} #{provider}\">#{result.html}</div>"
     end
   end
 end
